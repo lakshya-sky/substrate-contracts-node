170c37e74e166aa4721d3f1d00f5647a059b6c61
lakshya-sky <8559992+lakshya-sky@users.noreply.github.com>
2022-12-16 19:51:25 -0500

    remove parity and use lakshya-sky

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -337,7 +337,7 @@ impl pallet_contracts::Config for Runtime {
 	type CallStack = [pallet_contracts::Frame<Self>; 31];
 	type WeightPrice = pallet_transaction_payment::Pallet<Self>;
 	type WeightInfo = pallet_contracts::weights::SubstrateWeight<Self>;
-	type ChainExtension = pallet_assets_chain_extension::substrate::AssetsExtension;
+	type ChainExtension = ();
 	type DeletionQueueDepth = DeletionQueueDepth;
 	type DeletionWeightLimit = DeletionWeightLimit;
 	type Schedule = Schedule;
